34bea50b5441ff309d61ce2d1debc20622398f77
zahidewan <zahidewan@gmail.com>
2018-04-22 23:29:44 -0500

    post update

---
+++ default.js
@@ -0,0 +1,14 @@
+var initGame = function () {
+    var cfg = {
+        draggable: true,
+        position: 'start',
+        onDrop: handleMove,
+    };
+
+    board = new ChessBoard('gameBoard', cfg);
+    game = new Chess();
+}
+
+var handleMove = function(source, target) {
+    var move = game.move({from: source, to: target});
+}
